74982027f1834984092288db9e41ff74a2b8c218
Matthew Pagan <mastapegs01@gmail.com>
2020-12-30 15:14:35 -0500

    created state

--- src/scripts/pages/SearchPage.ts
+++ src/scripts/pages/SearchPage.ts
@@ -1,11 +1,25 @@
-import { LitElement,  html, customElement } from "lit-element";
+import { LitElement, property, html, customElement } from "lit-element";
 
 @customElement("search-page")
 export class AboutPage extends LitElement {
+  @property() user = "";
+
+  handleUserChange(event) {
+    this.user = event.target.value;
+  }
 
   render() {
     return html`
-       <h1>Github User Search</h1>
+      <h1>Github User Search</h1>
+      <div>
+        <label for="user">User:</label>
+        <input
+          id="user"
+          .value="${this.user}"
+          @input="${this.handleUserChange}"
+        />
+      </div>
+      <p>User: ${this.user}</p>
     `;
   }
 }
