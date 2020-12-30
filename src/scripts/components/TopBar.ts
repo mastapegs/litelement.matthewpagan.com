fcadc9f64eaeeded877498688156724c7381f074
Matthew Pagan <mastapegs01@gmail.com>
2020-12-30 13:02:40 -0500

    more exporting of default styles

--- src/scripts/components/TopBar.ts
+++ src/scripts/components/TopBar.ts
@@ -1,24 +1,23 @@
 import { LitElement, html, css, customElement } from "lit-element";
 import resetList from "../styles/reset-list";
+import flex from "../styles/flex";
+import anchorReset from "../styles/anchor-reset";
 
 @customElement("top-bar")
 export class TopBar extends LitElement {
   static get styles() {
     return [
       resetList,
+      flex,
+      anchorReset,
       css`
-        nav {
-          background-color: blue;
-        }
         ul {
-          display: flex;
+          background-color: blue;
         }
         a {
           display: inline-block;
           padding: 0.5em;
           margin: 1em 0 1em 1em;
-          color: white;
-          text-decoration: inherit;
           background-color: hsl(210, 100%, 50%);
           border-radius: 30%;
         }
@@ -29,10 +28,10 @@ export class TopBar extends LitElement {
   render() {
     return html`
       <nav>
-        <ul class="reset-list">
-          <li><a href="/">Home</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/contact">Contact</a></li>
+        <ul class="reset-list flex">
+          <li><a class="anchor-reset" style="color: white;" href="/">Home</a></li>
+          <li><a class="anchor-reset" href="/about">About</a></li>
+          <li><a class="anchor-reset" href="/contact">Contact</a></li>
         </ul>
       </nav>
     `;
---
+++ src/scripts/styles/anchor-reset.ts
@@ -0,0 +1,8 @@
+import { css } from "lit-element";
+
+export default css`
+  .anchor-reset {
+    color: inherit;
+    text-decoration: inherit;
+  }
+`;
