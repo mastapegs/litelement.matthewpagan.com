eb744bae974e60b0174e5a08862afe71acb63a31
Matthew Pagan <mastapegs01@gmail.com>
2020-12-30 12:40:01 -0500

    create a proper container for pages

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,13 +1,26 @@
-import { LitElement, html, customElement } from "lit-element";
+import { LitElement, html, css, customElement } from "lit-element";
 import "./components/TopBar";
 import './components/AppRouter'
 
 @customElement("app-component")
 export class AppComponent extends LitElement {
+  static get styles() {
+    return css`
+      .container {
+        background-color: white;
+        padding: 1em;
+        width: 80%;
+        margin: 1em auto;
+        border-radius: 10%;
+        box-shadow: 3px 5px 10px hsla(0, 0%, 50%, 0.5)
+      }
+    `
+  }
+  
   render() {
     return html`
       <top-bar></top-bar>
-      <div>
+      <div class="container">
         <app-router></app-router>
       </div>
     `;
