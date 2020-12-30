922add041e13d93a8f730fff3b72d81c1b1aac48
Matthew Pagan <mastapegs01@gmail.com>
2020-12-30 12:44:44 -0500

    create container utility css

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,22 +1,14 @@
-import { LitElement, html, css, customElement } from "lit-element";
+import { LitElement, html, customElement } from "lit-element";
 import "./components/TopBar";
-import './components/AppRouter'
+import "./components/AppRouter";
+import container from "./styles/container";
 
 @customElement("app-component")
 export class AppComponent extends LitElement {
   static get styles() {
-    return css`
-      .container {
-        background-color: white;
-        padding: 1em;
-        width: 80%;
-        margin: 1em auto;
-        border-radius: 10%;
-        box-shadow: 3px 5px 10px hsla(0, 0%, 50%, 0.5)
-      }
-    `
+    return [container];
   }
-  
+
   render() {
     return html`
       <top-bar></top-bar>
---
+++ src/scripts/styles/container.ts
@@ -0,0 +1,12 @@
+import { css } from "lit-element";
+
+export default css`
+  .container {
+    background-color: white;
+    padding: 1em;
+    width: 80%;
+    margin: 1em auto;
+    border-radius: 10%;
+    box-shadow: 3px 5px 10px hsla(0, 0%, 50%, 0.5);
+  }
+`;
